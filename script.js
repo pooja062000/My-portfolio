98e02b1613cfd4c3f8f90b8388152277e1151c68
pooja062000 <vikaskosli9890@gmail.com>
2023-02-02 22:56:57 +0530

    push

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ document.querySelector('#close_menu').addEventListener('click', function(){
     document.querySelector('.my_mobile').style.display = 'none'
 })
 
+
+/* home text moving code*/
+var typing=new Typed(".text", {
+    strings: [" Frontend Developer", "Freelancer", "Web Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
+
+
+  
+  
+  
